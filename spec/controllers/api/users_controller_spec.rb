745306f94b873d62f22f609fae8870dbc39417fe
Denis Stoica <denis.stoica@takeofflabs.com>
2022-09-13 16:02:25 +0300

    Add tests for show method

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe Api::UsersController, type: :controller do
+  before :each do
+    @user1 = create(:user, email: 'usertest@email.com', password: 'userpass')
+    # sign_in(@user1, scope: :user)
+  end
+
   describe 'POST login' do
     before :each do
       create(:user, email: 'user@email.com', password: 'userpass')
@@ -20,6 +25,25 @@ describe Api::UsersController, type: :controller do
       post :login, params: { email: 'invalid', password: 'user' }
 
       expect(response).to have_http_status(401)
+    end
+  end
+
+  describe 'GET show' do
+    it 'should return unauthorized if not logged in' do
+      get :show, params: {
+        id: @user1.id
+      }
+
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+    it 'should return ok if golfer logged in' do
+      sign_in(@user1)
+      get :show, params: {
+        id: @user1.id
+      }
+
+      expect(response).to have_http_status(:ok)
     end
   end
 end
